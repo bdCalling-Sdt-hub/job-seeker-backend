08bae00e96701d329c871f31207010bedb19e882
AbdurRahman4991 <engrabdurrahman4991@gmail.com>
2024-02-09 09:41:23 +0600

    add contact api

--- routes/api.php
+++ routes/api.php
@@ -1,35 +1,24 @@
 <?php
 
+use App\Http\Controllers\Api\Webapi\ContactController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::group([
-
     ['middleware' => 'auth:api']
-
 ], function ($router) {
-
-    Route::post("/register", [AuthController::class, "register"]);
-    Route::post("/email-verified", [AuthController::class, "emailVerified"]);
-    Route::post("/login", [AuthController::class, "login"]);
-    Route::get("/profile", [AuthController::class, "loggedUserData"]);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/email-verified', [AuthController::class, 'emailVerified']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/profile', [AuthController::class, 'loggedUserData']);
     Route::post('forget-pass', [AuthController::class, 'forgetPassword']);
     Route::post('/verified-checker', [AuthController::class, 'emailVerifiedForResetPass']);
     Route::post('/reset-pass', [AuthController::class, 'resetPassword']);
     Route::post('/update-pass', [AuthController::class, 'updatePassword']);
-    Route::put("/profile/edit/{id}", [AuthController::class, 'editProfile']);
+    Route::put('/profile/edit/{id}', [AuthController::class, 'editProfile']);
 });
 
-//Route::post("/register", [AuthController::class, "register"]);
+// Route::post("/register", [AuthController::class, "register"]);
 
+Route::post('/contact', [ContactController::class, 'contact']);
