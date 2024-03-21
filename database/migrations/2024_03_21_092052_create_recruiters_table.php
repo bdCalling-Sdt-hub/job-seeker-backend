4978e50afb28940d6a7b762b2bb37f7c633444bc
MdJusef <mdjusef143@gmail.com>
2024-03-21 16:35:44 +0600

    update

--- database/migrations/2024_03_21_092052_create_recruiters_table.php
+++ database/migrations/2024_03_21_092052_create_recruiters_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('candidates', function (Blueprint $table) {
+        Schema::create('recruiters', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
